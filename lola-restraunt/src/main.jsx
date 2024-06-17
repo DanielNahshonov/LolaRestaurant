65b2f921964aba0e98db643f24547249263306bb
DanielNahshonov <daniel.n@circ.zone>
2024-06-17 13:11:43 +0300

    push

--- lola-restraunt/src/main.jsx
+++ lola-restraunt/src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter , Routes, Route} from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/LolaRestaurant">
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
